2380b3a27ac99cb1f7499d08cf1a0ef3a275b6f1
Dayoung Kang <alledy1016@gmail.com>
2020-10-26 09:48:15 +0900

    Upload 'convert to postfix' problem from Inflearn

--- inflearn/스택&큐/후위표기식 만들기.py
+++ inflearn/스택&큐/후위표기식 만들기.py
@@ -1,22 +1,27 @@
-# def get_postfix(pref):
-#     stack = []
-#     ans = ''
-#     for x in pref:
-#         if x.isdecimal():
-#             ans += x
-#         else:
-#             if x == '*' or x == '/': 
-#                 while stack and (stack[-1] == '*' or stack[-1] == '/'):
-#                     ans += stack.pop()
-#                 stack.append(x)
-#             elif x == '+' or x == '-':
-#                 while stack and (stack[-1] == '+' or stack[-1] == '-'):
-#                     ans += stack.pop()
-#                 stack.append(x)
-#             elif x == '(':
-#                 stack.append(x)
-#             elif x == ')':
-
+def get_postfix(pref):
+    stack = []
+    ans = ''
+    for x in pref:
+        if x.isdecimal():
+            ans += x
+        else:
+            if x == '*' or x == '/': 
+                while stack and (stack[-1] == '*' or stack[-1] == '/'):
+                    ans += stack.pop()
+                stack.append(x)
+            elif x == '+' or x == '-':
+                while stack and stack[-1] != '(':
+                    ans += stack.pop()
+                stack.append(x)
+            elif x == '(':
+                stack.append(x)
+            elif x == ')':
+                while stack[-1] != '(':
+                    ans += stack.pop()
+                stack.pop()
+    while stack:
+        ans+= stack.pop()
+    return ans
 
                     
 

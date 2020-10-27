dac5533b0d2803a29d6d52f7cb11194a831f20af
Dayoung Kang <alledy1016@gmail.com>
2020-10-27 10:48:24 +0900

    Upload 'Emergency room' from Inflearn

---
+++ inflearn/스택&큐/응급실.py
@@ -0,0 +1,14 @@
+# arr은 전체 위험도가 담긴 num[]
+# m은 m번째 인덱스 환자가 몇번째로 치료를 받는지 구해야 하는 것
+# import heapq
+# def emergency(arr,m):
+#     h = []
+#     for i, x in enumerate(arr):
+#         heapq.heappush(h,(-x, i))
+#     for i, x in enumerate(h):
+#         if x[1] == m:
+#             return i+1
+                    
+
+    
+    
